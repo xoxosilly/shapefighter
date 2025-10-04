d157695ced179ff3d63a5b67e6edf7d78dc0d337
xoxosilly <slayallday2938@gmail.com>
2025-10-04 18:35:47 -0500

    Create tutorial.js

---
+++ src/tutorial.js
@@ -0,0 +1,9 @@
+export function showTutorial() {
+  alert(
+    "Welcome to Shape Fighter!\n" +
+    "Use A to attack. Defeat bots to earn coins.\n" +
+    "Visit the Shop to buy new weapons/power-ups.\n" +
+    "Upgrade your shape and unlock new skins.\n" +
+    "Check Achievements, Stats, and Leaderboard in the menu."
+  );
+}
